9975883452cec4c79c4f5a75ea75fdac0cf1721c
Yao-Min Huang <yaomin.huang@gmail.com>
2013-10-16 13:39:35 +0800

    fix missing rpcnetp.exe

--- ymh_del_CTagents.bat
+++ ymh_del_CTagents.bat
@@ -1,6 +1,5 @@
 del c:\ymh\dbg*.txt
 taskkill /im rpcnetp.exe /f
-del c:\windows\system32\rpcnetp.*
-del c:\windows\syswow64\rpcnetp.*
 taskkill /im rpcnet.exe /f
-del c:\windows\syswow64\rpcnet.*
+del c:\windows\system32\rpcnet*.*
+del c:\windows\syswow64\rpcnet*.*
--- ymh_list_CTagents.bat
+++ ymh_list_CTagents.bat
@@ -1,4 +1,4 @@
-dir c:\windows\system32\rpcnetp.*
+dir c:\windows\system32\rpcnet*.*
 dir c:\windows\syswow64\rpcnet*.*
 tasklist /FI "IMAGENAME EQ rpcnetp.exe"
 tasklist /FI "IMAGENAME EQ rpcnet.exe"
