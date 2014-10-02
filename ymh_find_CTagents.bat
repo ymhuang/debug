90a6a2b354d9586eec118bbc73d50ced6974f09c
ymhuang <yaomin.huang@gmail.com>
2014-10-03 02:34:55 +0800

    Delete ymh_find_CTagents.bat

--- ymh_find_CTagents.bat
+++
@@ -1,6 +0,0 @@
-tasklist /FI "STATUS eq running" >> c:\ymh\dbg.txt
-dir c:\windows\system32\rpcnetp.* >> c:\ymh\dbg.txt
-dir c:\windows\syswow64\rpcnet*.* >> c:\ymh\dbg.txt
-dir C:\windows\SysWow64\Upgrd.exe >> c:\ymh\dbg.txt
-tasklist /FI "IMAGENAME EQ rpcnetp.exe" >> c:\ymh\dbg.txt
-tasklist /FI "IMAGENAME EQ rpcnet.exe" >> c:\ymh\dbg.txt
