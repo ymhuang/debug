d1f76c6f5b7b4474dd0539f2f82c4b3a059246ce
Yao-Min Huang <yaomin.huang@gmail.com>
2015-12-31 15:44:18 +0800

    Update WaitForEnter()

--- UlToStr.c
+++ UlToStr.c
@@ -13,13 +13,38 @@ void UlToStr(CHAR16 *s, unsigned long bin, unsigned char n)
 
 void CharToStr(CHAR16 *s, char *c)
 {
-	int i = 0;
+    int i = 0;
     while (c[i] != '\0')
     {
         *(s+i) = c[i];
-		i++;
-		*(s+i) = '\0';
+        i++;
+	*(s+i) = '\0';
     }
+}
+
+void WaitForEnter ( void ) 
+{
+    EFI_INPUT_KEY key;
+    char * msg = "Press ENTER to continue";
+    CHAR16 s[32];
+    CHAR16 n[2] = {'\n', '\r'};
+
+    CharToStr( s, msg );
+    ST->ConOut->OutputString(ST->ConOut, s );
+    ST->ConOut->OutputString(ST->ConOut, n );
+
+    do
+    {
+        if ( ST->ConIn->ReadKeyStroke( ST->ConIn, &key ) == 0 )
+        {
+            if (key.UnicodeChar == 0x0D)
+	    {
+                break;
+            }
+        }
+    } while (1);
+
+    return;
 }
 
 void main()
